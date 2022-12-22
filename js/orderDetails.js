9764ee8e98fb60aa25d59c15f7557fcd92fb0bd8
micky rendon <elderendon@hotmail.es>
2022-12-22 12:52:02 -0300

    deleted files

--- js/orderDetails.js
+++
@@ -1,6 +0,0 @@
-// product details close btn
-export const close = () => {
-    const orderDetails =  document.querySelector('.details-ctr')
-    orderDetails.classList.add('hidden')
-}
-// TODO, acoplar con los otros menus para que se abra solo uno a la vez
